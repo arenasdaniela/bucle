46fdcbe19599cdf9b93bd72f70d2b514111df670
arenasdaniela <135679504+arenasdaniela@users.noreply.github.com>
2023-06-05 21:44:53 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+  int numero, i;
+    cout<<"ingrese un numero: ";
+    cin>> numero;
+  for(i=1; i<=12; i++)
+    {
+      cout<<numero<<"x"<<i<<"="<<numero*i<<endl;
+    }
+}
+  
+
